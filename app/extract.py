f1abe29122fe32e8e10509b4aadd762caa8cd011
Thomás Freire <thomas.fsr@gmail.com>
2024-03-16 14:37:50 -0300

    request api coin market cap

--- app/extract.py
+++ app/extract.py
@@ -1,13 +1,11 @@
 from dotenv import load_dotenv
-import coinmarketcapapi
+from coinmarketcapapi import CoinMarketCapAPI
 import os
 
 load_dotenv()
 
 api_key = os.getenv('api_key')
 
-cmc_client = coinmarketcapapi.CoinMarketCapAPI(api_key)
+cmc_client = CoinMarketCapAPI(api_key)
 response = cmc_client.cryptocurrency_listings_latest()
 print(response.data)
-
-
