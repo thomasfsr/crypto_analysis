aa30368408d7bc0f3f244b96dd2cc5414b68fa64
Thomás Freire <thomas.fsr@gmail.com>
2024-03-16 14:45:52 -0300

    without api key for coin market cap analysis

--- app/extract.py
+++ app/extract.py
@@ -1,11 +1,5 @@
-from dotenv import load_dotenv
 from coinmarketcapapi import CoinMarketCapAPI
-import os
 
-load_dotenv()
-
-api_key = os.getenv('api_key')
-
-cmc_client = CoinMarketCapAPI(api_key)
+cmc_client = CoinMarketCapAPI()
 response = cmc_client.cryptocurrency_listings_latest()
 print(response.data)
